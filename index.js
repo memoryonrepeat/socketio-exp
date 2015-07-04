f5ddd84c3e12fd6f5a5bd4f2f6a4761257f6de40
Thanh Nguyen <tienthanh8490@gmail.com>
2015-07-05 01:20:34 +0800

    basic chat

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+TODO:
+Broadcast a message to connected users when someone connects or disconnects
+Add support for nicknames
+Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+Add “{user} is typing” functionality
+Show who’s online
+Add private messaging
+Share your improvements!
+*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
